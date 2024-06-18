6aa2c8f4549b2d186c131e6a8a7b68c7502547e6
leonkosnar <leon@kosnar.at>
2024-06-18 12:51:25 +0200

    client: login/signup and listing products

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -63,3 +63,100 @@ function filter(elmnt, category){
             // Handle error scenario, show error message to user, etc.
         }
     }
+
+
+
+    // FORMS
+
+    const validate_login = () => {
+        if(document.forms['login-form']['email'].value === '' || document.forms['login-form']['password'].value === ''){
+            alert('Please fill in all fields');
+            return false;
+        }
+    }
+
+    const validate_register = () => {
+        if(document.forms['register-form']['email'].value === ''
+            || document.forms['register-form']['password'].value === ''
+            || document.forms['register-form']['firstname'].value === ''
+            || document.forms['register-form']['lastname'].value === ''
+            || document.forms['register-form']['address'].value === ''
+        ){
+            alert('Please fill in all fields');
+            return false;
+        }
+        if(document.forms['register-form']['password'].value != document.forms['register-form']['password2'].value
+        ){
+            alert('The passwords do not match!');
+            return false;
+        }
+    }
+
+    const login = () => {
+        fetch('/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: document.forms['login-form']['email'].value,
+                password: document.forms['login-form']['password'].value,
+            }),
+        }).then((response) => {
+            console.log(response);
+            if(!response.ok){
+                alert("The login attempt has failed!");
+            }
+        }).catch(error => {
+            console.error(error);
+            alert("There has been an error!");
+        })
+    }
+
+    const register = () => {
+        fetch('/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: document.forms['register-form']['email'].value,
+                password: document.forms['register-form']['password'].value,
+                firstname: document.forms['register-form']['firstname'].value,
+                lastname: document.forms['register-form']['lastname'].value,
+                address: document.forms['register-form']['address'].value,
+            }),
+        }).then((response) => {
+            console.log(response);
+            if(!response.ok){
+                alert("The signup attempt has failed!");
+            }
+        }).catch(error => {
+            console.error(error);
+            alert("There has been an error!");
+        })
+    }
+
+    window.onload = () => {
+        const grid = document.querySelector("#grid .grid-container")
+        const scroll = document.querySelector("#scroll .h-scroll-wrapper")
+        fetch('/api/products').then((resp) => {
+            if(!resp.ok){
+                alert("There has been an error!");
+            }
+            return resp.json();
+        }).then((products) => {
+            console.log(products);
+            products.forEach((product, index) => {
+                grid.innerHTML += `
+                <button popovertarget="product_${index}">
+                    <div class="grid-item grid-item-xl">
+                        <img src="${product.images[0]}" alt="${product.title}">
+                        <div class="overlay">${product.title}</div>
+                    </div>
+                    <div id="product_${index}" class="product_popover" popover>${product.title}</div>
+                </button>
+                `
+            })
+        })
+    }
