7900558b01a002d428e37673a6154dabac282bcb
leonkosnar <leon@kosnar.at>
2024-06-24 18:39:51 +0200

    added grayscale to non-focused products and improved sidebar

--- public/scripts/hamburger.js
+++ public/scripts/hamburger.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const sidebar = document.getElementById('sidebar');
     const closeSidebar = document.getElementById('close-sidebar');
+    const menuItems = document.getElementById('menu-items');
+    const menuItemsSidebar = document.getElementById('menu-items-sidebar');
 
     hamburgerMenu.addEventListener('click', function () {
         sidebar.classList.toggle('active');
+        menuItemsSidebar.innerHTML = menuItems.innerHTML;
+        menuItems.innerHTML = "";
     });
 
     closeSidebar.addEventListener('click', function () {
         sidebar.classList.remove('active');
+        menuItems.innerHTML = menuItemsSidebar.innerHTML;
+        menuItemsSidebar.innerHTML = "";
     });
 });
